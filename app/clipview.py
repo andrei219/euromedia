21db2cd797f85e14ef4d8ce5ee6b10e348f1c66a
Andrei Enache <andrei.officee@gmail.com>
2022-07-14 16:44:05 +0200

    Implement ClipListView

--- app/clipview.py
+++ app/clipview.py
@@ -1,7 +1,10 @@
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QTableView
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QTableView
 from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QListView
+
+
 
 
 from clipboard import ClipBoard
@@ -28,4 +31,14 @@ class ClipLineEdit(QLineEdit):
             super().mousePressEvent(e)
 
     def contextMenuEvent(self, a0: QtGui.QContextMenuEvent) -> None:
-        pass 
+        pass
+
+class ClipListView(QListView):
+
+    def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
+        if e.button() == Qt.RightButton:
+            current = self.selectionModel().currentIndex()
+            data = self.model().data(current, role=Qt.DisplayRole)
+            ClipBoard().data = data
+        else:
+            super().mousePressEvent(e)
