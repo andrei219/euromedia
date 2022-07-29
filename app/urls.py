f2fe025053e8f0c717dd1d1158204a2892b81376
Andrei Enache <andrei.officee@gmail.com>
2022-07-29 09:33:49 +0200

    cleaning directory

--- app/urls.py
+++
@@ -1,6 +0,0 @@
-
-
-engine_urls = {
-    'Euromedia':'mysql+mysqlconnector://root:hnq#4506@localhost:3306/euromediadb', 
-    'Mobify':'mysql+mysqlconnector://root:hnq#4506@localhost:3306/mobifydb'
-}
