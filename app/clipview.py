ff4215c449df85a5072269f2bcd5e47b621cc0e1
Andrei Enache <andrei.officee@gmail.com>
2022-08-26 17:34:55 +0200

    Use System wide clipboard insetad of local

--- app/clipview.py
+++ app/clipview.py
@@ -3,10 +3,15 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QTableView
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QListView
+from alembic.command import current
 
 from clipboard import ClipBoard
 
-class ClipView(QTableView):
+
+
+import PyQt5.QtCore
+
+class ClipView_old(QTableView):
 
     def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
         if e.button() == Qt.RightButton:
@@ -18,11 +23,34 @@ class ClipView(QTableView):
             super().mousePressEvent(e)
 
 
-class ClipLineEdit(QLineEdit):
+class ClipView(QTableView):
 
     def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
         if e.button() == Qt.RightButton:
-            self.setText(ClipBoard().data)
+            app = PyQt5.QtCore.QCoreApplication.instance()
+            local_clipboard = ClipBoard()
+            current = self.selectionModel().currentIndex()
+            data = self.model().data(current, role=Qt.DisplayRole)
+            local_clipboard.data = data
+            app.clipboard().setText(data)
+
+        else:
+            super().mousePressEvent(e)
+
+class ClipLineEdit(QLineEdit):
+
+    def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
+        print('Mosuse Press event catched')
+        if e.button() == Qt.RightButton:
+            app = PyQt5.QtCore.QCoreApplication.instance()
+            global_text = app.clipboard().text()
+            if global_text:
+                print
+                self.setText(global_text)
+            else:
+                local_text = ClipBoard()
+                if local_text:
+                    self.setText(local_text)
         else:
             super().mousePressEvent(e)
 
@@ -34,8 +62,13 @@ class ClipListView(QListView):
 
     def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
         if e.button() == Qt.RightButton:
+            app = PyQt5.QtCore.QCoreApplication.instance()
+            local_clipboard = ClipBoard()
             current = self.selectionModel().currentIndex()
             data = self.model().data(current, role=Qt.DisplayRole)
-            ClipBoard().data = data
+            local_clipboard.data = data
+            app.clipboard().setText(data)
         else:
             super().mousePressEvent(e)
+
+
