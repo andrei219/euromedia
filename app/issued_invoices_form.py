52583fc408597bc5f86ed269bdc8f9035a0f459b
Andrei Enache <andrei.officee@gmail.com>
2022-09-23 16:37:56 +0200

    Invoice lines corrected

--- app/issued_invoices_form.py
+++ app/issued_invoices_form.py
@@ -3,7 +3,7 @@ from ui_issued_invoices_form import Ui_Dialog
 
 from PyQt5.QtWidgets import QDialog, QMessageBox
 
-from issued_invoices_pdf_builder import build_document
+from issued_invoices_pdf_builder import build_document, PDF
 from datetime import datetime
 
 import utils
@@ -48,6 +48,23 @@ class Filters:
         return str(self.__dict__.values())
 
 
+class Register:
+
+    def __init__(self, invoice):
+        self.doc_repr = invoice.doc_repr
+        self.date = invoice.date.strftime('%d/%m/%Y')
+        self.partner = invoice.partner_name
+        self.agent = invoice.agent
+        self.financial = invoice.financial_status_string
+        self.subtotal = str(invoice.subtotal)
+        self.tax = str(invoice.tax)
+        self.debt = str(invoice.total_debt)
+        self.currency = 'EUR' if invoice.proformas[0].eur_currency else 'USD'
+
+    def __str__(self):
+        return f"{self.doc_repr:12}{self.date:14}{self.partner:40}{self.agent:14}{self.financial:16}{self.subtotal:10}{self.tax:10}{self.debt:10}{self.currency:10}"
+
+
 class PDFContent:
 
     __slots__ = ('from_', 'to', 'registers', 'created_on')
@@ -57,23 +74,11 @@ class PDFContent:
         self.to = filters.to
         self.created_on = datetime.now()
 
-        self.registers = [
-            (
-                invoice.doc_repr,
-                invoice.date.strftime('%d/%m/%Y'),
-                invoice.partner_name, invoice.agent,
-                invoice.financial_status_string,
-                invoice.subtotal,
-                invoice.tax,
-                invoice.total_debt,
-                'EUR' if invoice.proformas[0].eur_currency else 'USD'
-            )
+        self.registers = [Register(invoice) for invoice in self._build_query(filters)]
 
-            for invoice in self._build_query(filters)
-        ]
-
-        for register in self.registers:
-            print(register)
+        with open('test.txt', 'w') as f:
+            for r in self.registers:
+                f.write(str(r))
 
     def _build_query(self, filters):
         query = db.session.query(db.SaleInvoice).join(db.SaleProforma).\
@@ -112,19 +117,16 @@ class Form(Ui_Dialog, QDialog):
             getattr(self, 'serie_' + str(serie)).setChecked(checked)
 
     def view_handler(self):
-        # subprocess bla bla bla
         pdf_document = self._build_report()
 
-
     def export_handler(self):
+        # file_path = utils.get_file_path(self, pdf_filter=True)
+        # if not file_path:
+        #     return
 
-        # Ask filename with pdf filters
+        pdf_document = self._build_report()
 
-        file_path = utils.get_file_path(self, pdf_filter=True)
-
-        print(file_path)
-
-        # pdf_document = self._build_report()
+        pdf_document.output(r'C:\Users\Andrei\Desktop\test.pdf')
 
 
     def _build_report(self):
--- app/issued_invoices_pdf_builder.py
+++ app/issued_invoices_pdf_builder.py
@@ -1,10 +1,66 @@
 
 from fpdf import FPDF
 
+from datetime import datetime
+
+TITLE = 90, 10
+
+EUROMEDIA = 10, 40
+FROM = 10, 48
+TO = 10, 56
+
+GENERATED = 120, 40
+
+TABLE_X = 8
+TABLE_Y = 70
+DATE = 20
+PARTNER = 30
+AGENT = 80
+FINANCIAL = 90
+
+TABLE_INNER_START = 10, 80
+
+ROW_INCREMENT = 5
+
 class PDF(FPDF):
 
     def __init__(self, content):
+        super().__init__()
+        self.content = content
+
+    def print_header(self):
+        self.x, self.y = TITLE
+        self.set_font('Arial',  'B', 18)
+        self.cell(0, txt='Issued Invoices')
+
+        self.set_font('Arial', size=10)
+        self.x, self.y = EUROMEDIA
+        self.cell(0, txt='Company: Euromedia Investment Group, S.L.')
+
+        self.x, self.y = FROM
+        self.cell(0, txt=f'From: {self.content.from_.strftime("%d/%m/%Y")}')
+
+        self.x, self.y = TO
+        self.cell(0, txt=f'To: {self.content.to.strftime("%d/%m/%Y")}')
+
+        self.x, self.y = GENERATED
+        self.set_font('Arial', 'B', size=10)
+        self.cell(0, txt=f"Generated at: {datetime.now().strftime('%m/%d/%Y, %H:%M:%S')}")
+
+        self.set_font('Arial', 'B', size=6)
+
+
+    def print_footer(self):
         pass
+
+    def print_body(self):
+        self.set_font('Courier', '', size=6)
+        x, y = TABLE_INNER_START
+        for register in self.content.registers:
+            self.text(x, y, str(register))
+            y += ROW_INCREMENT
+
+
 
 
 def build_document(content):
@@ -12,6 +68,7 @@ def build_document(content):
     pdf.alias_nb_pages()
     pdf.set_auto_page_break(True, 10)
     pdf.add_page()
+    pdf.print_header()
     pdf.print_body()
     pdf.print_footer()
     return pdf
