d03d1553ce05c6ebdd5ec3d5624fe28cfb457509
Andrei Enache <andrei.officee@gmail.com>
2023-02-06 10:03:29 +0100

    Raise test error

--- app/maingui.py
+++ app/maingui.py
@@ -1505,6 +1505,10 @@ class MainGui(Ui_MainGui, QMainWindow):
 
     # TOOLS HANDLERS:
     def tools_trace_handler(self):
+
+        raise ValueError('Test error')
+        
+
         from trace_form import Form
         f = Form(self)
         f.exec_()
