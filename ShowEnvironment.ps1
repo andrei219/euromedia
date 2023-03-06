50ecd1aacfb8ad35d30f3275c5d7028d51c1fc11
Andrei Enache <andrei.officee@gmail.com>
2023-03-06 11:56:30 +0100

    Show environment: Show branch

--- ShowEnvironment.ps1
+++ ShowEnvironment.ps1
@@ -5,3 +5,4 @@ Write-Host "`$env:APP_ECHO=$env:APP_ECHO"
 Write-Host "`$env:APP_MAIL_PASSWORD=$env:APP_MAIL_PASSWORD"
 Write-Host "`$env:GITHUB_AUTH=$env:GITHUB_AUTH"
 
+Write-Host "Current branch: $(git branch --show-current)"
