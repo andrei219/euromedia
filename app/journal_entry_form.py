a9790ee0ca78966c4dfb914c08b4e86c2ad5ad00
Andrei Enache <andrei.officee@gmail.com>
2023-03-06 10:22:00 +0100

    Commit on accounting

--- app/journal_entry_form.py
+++ app/journal_entry_form.py
@@ -4,11 +4,20 @@ from ui_journal_entry_form import Ui_Form
 from PyQt5.QtWidgets import QWidget
 
 
+from utils import setCompleter
+
+from db import Account, session
+
+
 class Form(Ui_Form, QWidget):
 
 	def __init__(self):
 		super().__init__()
 		self.setupUi(self)
+
+		data = [f'{r.code} - {r.name}' for r in session.query(Account)]
+
+		setCompleter(self.description, data)
 
 		self.save.clicked.connect(self.save_handler)
 
