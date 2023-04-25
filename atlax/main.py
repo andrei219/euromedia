abdc8dc288b810da13c9d89d603095f33249f07b
Andrei Enache <andrei.officee@gmail.com>
2023-04-25 13:41:05 +0200

    some work on atlax

---
+++ atlax/main.py
@@ -0,0 +1,8 @@
+
+import os, sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+
+from app.db import *
+
