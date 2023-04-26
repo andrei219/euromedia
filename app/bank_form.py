95b1fed99950ee6cbe6b9975ba84af122c7d7c11
Andrei Enache <andrei.officee@gmail.com>
2023-04-26 09:35:34 +0200

    Atlax model with test data finished

--- app/bank_form.py
+++ app/bank_form.py
@@ -1,109 +1,109 @@
 
 
 from PyQt5.QtWidgets import (
-    QDialog,
-    QTableView,
-    QMessageBox 
+	QDialog,
+	QTableView,
+	QMessageBox
 )
 
 from PyQt5.QtCore import QStringListModel
 
-from ui_bank_form import Ui_Dialog 
+from ui_bank_form import Ui_Dialog
 
-import utils 
-    
-import db 
-import models 
+import utils
+
+import db
+import models
 
 from PyQt5.QtCore import Qt
 
 
 class Dialog(Ui_Dialog, QDialog):
 
-    def __init__(self, parent, partner):
-        super().__init__(parent=parent)
-        self.partner = partner 
-        self.setupUi(self)
-        self.partner = partner 
-        self.model = models.BankAccountModel(partner)
-        self.view.setModel(self.model) 
+	def __init__(self, parent, partner):
+		super().__init__(parent=parent)
+		self.partner = partner
+		self.setupUi(self)
+		self.partner = partner
+		self.model = models.BankAccountModel(partner)
+		self.view.setModel(self.model)
 
-        self.set_handlers()
+		self.set_handlers()
 
-        self.country.addItems(utils.countries)
+		self.country.addItems(utils.countries)
 
-        self.view.setSelectionBehavior(QTableView.SelectRows)
-        self.view.setSelectionMode(QTableView.SingleSelection)
+		self.view.setSelectionBehavior(QTableView.SelectRows)
+		self.view.setSelectionMode(QTableView.SingleSelection)
 
-        self.view.setAlternatingRowColors(True)
+		self.view.setAlternatingRowColors(True)
 
-    def set_handlers(self):
-        self.add.clicked.connect(self.add_handler)
-        self.delete_.clicked.connect(self.delete_handler) 
-        self.save.clicked.connect(self.save_handler) 
-        self.exit.clicked.connect(lambda : self.close())    
-        self.iban.textChanged.connect(self.update_bic)
-
-
-    def update_bic(self, iban):
-        from utils import swiftFromIban
-        try:
-            print(iban)
-            bic = swiftFromIban(iban)
-            self.swift.setText(bic)
-        except: pass 
-        
-
-    def clear_fields(self):
-        
-        for field_name in [
-            'name', 'iban', 'swift', 'address', 
-            'postcode', 'city', 'state', 'routing_'
-        ]:
-            try:
-                getattr(self, field_name).clear()
-            except:
-                pass     
-
-    def add_handler(self):
-        try:
-            self.model.add(
-                self.name.text(), self.iban.text(), 
-                self.swift.text(), self.address.text(), 
-                self.postcode.text(), self.city.text(), 
-                self.state.text(), self.country.currentText(), 
-                self.routing_.text(), self.currency.text()
-            )
-
-            self.clear_fields()
-        except:
-            raise   
-    
+	def set_handlers(self):
+		self.add.clicked.connect(self.add_handler)
+		self.delete_.clicked.connect(self.delete_handler)
+		self.save.clicked.connect(self.save_handler)
+		self.exit.clicked.connect(lambda : self.close())
+		self.iban.textChanged.connect(self.update_bic)
 
 
-    def delete_handler(self):
-        rows = {
-            index.row() 
-            for index in self.view.selectedIndexes()
-        }
-        if not rows:return 
-        row = rows.pop() 
-        try:
-            self.model.delete(row)
-        except:
-            raise 
-        
-        self.view.clearSelection()
-
-    def save_handler(self):
-        try:
-            db.session.commit()
-            QMessageBox.information(self, 'Information', 'Accounts saved/updated successfully')
-        except:
-            raise 
+	def update_bic(self, iban):
+		from utils import swiftFromIban
+		try:
+			print(iban)
+			bic = swiftFromIban(iban)
+			self.swift.setText(bic)
+		except: pass
 
 
-    def closeEvent(self, event):
-        import db 
-        db.session.rollback() 
-        super().closeEvent(event)
+	def clear_fields(self):
+
+		for field_name in [
+			'name', 'iban', 'swift', 'address',
+			'postcode', 'city', 'state', 'routing_'
+		]:
+			try:
+				getattr(self, field_name).clear()
+			except:
+				pass
+
+	def add_handler(self):
+		try:
+			self.model.add(
+				self.name.text(), self.iban.text(),
+				self.swift.text(), self.address.text(),
+				self.postcode.text(), self.city.text(),
+				self.state.text(), self.country.currentText(),
+				self.routing_.text(), self.currency.text()
+			)
+
+			self.clear_fields()
+		except:
+			raise
+
+
+
+	def delete_handler(self):
+		rows = {
+			index.row()
+			for index in self.view.selectedIndexes()
+		}
+		if not rows:return
+		row = rows.pop()
+		try:
+			self.model.delete(row)
+		except:
+			raise
+
+		self.view.clearSelection()
+
+	def save_handler(self):
+		try:
+			db.session.commit()
+			QMessageBox.information(self, 'Information', 'Accounts saved/updated successfully')
+		except:
+			raise
+
+
+	def closeEvent(self, event):
+		import db
+		db.session.rollback()
+		super().closeEvent(event)
--- atlax/model.py
+++ atlax/model.py
@@ -10,9 +10,15 @@ from sqlalchemy.orm import relationship, sessionmaker, backref
 
 Base = declarative_base()
 
+
+engine = create_engine('sqlite:///atlax/atlax inicial.db')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
 class Sociedad(Base):
 
-	__tablename__ = 'empresas'
+	__tablename__ = 'sociedades'
 
 	id = Column(Integer, primary_key=True)
 	razons = Column(String(255), nullable=False)
@@ -41,21 +47,21 @@ class CondicionesPagoCobro(Base):
 	__tablename__ = 'condiciones_pago_cobro'
 
 	id = Column(Integer, primary_key=True)
-	cod = Column(String(64), nullable=False)
+	desc = Column(String(64), nullable=False)
 
 class Plazo(Base):
 
 	__tablename__ = 'plazos'
 
 	id = Column(Integer, primary_key=True)
-	condiciones_id = Column(Integer, ForeignKey('condiciones_pago_cobro.id'))
+	condicion_id = Column(Integer, ForeignKey('condiciones_pago_cobro.id'))
 	forma_pago_id = Column(Integer, ForeignKey('formas_pago_cobro.id'))
 
 	dias = Column(Integer, nullable=False)
 	porc = Column(Numeric(18, 2), nullable=False)
 
-	plazos = relationship('CondicionesPagoCobro', backref=backref('plazos', uselist=True))
 	forma_pago = relationship('FormasPagoCobro', uselist=False)
+	condicion = relationship('CondicionesPagoCobro', backref=backref('plazos', uselist=True))
 
 
 class Cliente(Base):
@@ -63,7 +69,7 @@ class Cliente(Base):
 	__tablename__ = 'clientes'
 
 	id = Column(Integer, primary_key=True)
-
+	sociedad_id = Column(Integer, ForeignKey('sociedades.id'), nullable=False)
 	condicion_id = Column(Integer, ForeignKey('condiciones_pago_cobro.id'), nullable=True)
 	clasificacion_id = Column(Integer, ForeignKey('clasificaciones.id'), nullable=True)
 	forma_pago_id = Column(Integer, ForeignKey('formas_pago_cobro.id'), nullable=True)
@@ -91,8 +97,18 @@ class Direccion(Base):
 	prov = Column(String(255), nullable=False)
 	pais = Column(String(255), nullable=False)
 
+	cliente = relationship('Cliente', backref=backref('direcciones', uselist=True))
+
+	def __init__(self, tdireccion, domicilio, ciudad, prov, pais):
+		super().__init__()
+		self.tdireccion = tdireccion
+		self.domicilio = domicilio
+		self.ciudad = ciudad
+		self.prov = prov
+		self.pais = pais
+
 	__table_args__ = (
-		CheckConstraint('tdireccion in (0, 1)')
+		CheckConstraint('tdireccion in (0, 1)'),
 	)
 
 
@@ -119,14 +135,14 @@ class PartidasCompensadas(Base):
 	cliente_id = Column(Integer, ForeignKey('clientes.id'), nullable=False)
 	forma_pago_id = Column(Integer, ForeignKey('formas_pago_cobro.id'), nullable=False)
 
-	numero_documento = Column(Integer, String(64), nullable=False)
+	numero_documento = Column(String(64), nullable=False)
 	fecha_emision = Column(Date, nullable=False)
 	fecha_vencimiento = Column(Date, nullable=False)
 	fecha_compensacion = Column(Date, nullable=False)
 	importe = Column(Numeric(18, 2), nullable=False)
 	tdoc = Column(String(32), nullable=False)
 
-	cliente = relationship('Cliente', backref=backref('partidas_abiertas', uselist=True))
+	cliente = relationship('Cliente', backref=backref('partidas_compensadas', uselist=True))
 	forma_pago = relationship('FormasPagoCobro', uselist=False)
 
 
@@ -140,7 +156,7 @@ class PartidasCompensadasAnuladas(Base):
 	fecha_inv = Column(Date, nullable=False)
 	fecha_creacion = Column(Date, nullable=False)
 
-	partida_compensada = Column('PartidasCompensadas', backref=backref('partidas_compensadas_anuladas', uselist=True))
+	partida_compensada = relationship('PartidasCompensadas', backref=backref('anulaciones', uselist=True))
 
 
 class ResumenVentas(Base):
@@ -157,11 +173,88 @@ class ResumenVentas(Base):
 	cliente = relationship('Cliente', uselist=False)
 
 
-engine = create_engine('sqlite:///atlax inicial.db')
-Base.metadata.create_all(engine)
+''' Function that generates random strings of lenght 9. First is letter, then digits'''
+def generate_random_nif():
+	import random, string
+	return random.choice(string.ascii_letters) + ''.join(random.choice(string.digits) for _ in range(8))
 
 
-Session = sessionmaker(bind=engine)
-session = Session()
+
+''' Function that creates sample data for these relations using Faker library. Also links the relationships '''
+def create_sample_data():
+
+	from faker import Faker
+	fake = Faker()
+	sociedad = Sociedad()
+	sociedad.razons = 'Sociedad de Prueba'
+	sociedad.nif = '12345678A'
+	sociedad.codmoneda = 'EUR'
+
+	session.add(sociedad)
+
+	clasificacion = Clasificacion()
+	clasificacion.desc = 'Criterio Unico'
+
+	session.add(clasificacion)
+
+	forma_pago = FormasPagoCobro()
+	forma_pago.desc = 'Transferencia'
+
+	session.add(forma_pago)
+
+	condicion = CondicionesPagoCobro()
+	condicion.desc = 'Contado'
+
+	session.add(condicion)
+
+	plazo = Plazo()
+
+	plazo.condicion = condicion
+	plazo.forma_pago = forma_pago
+	plazo.dias = 0
+	plazo.porc = 100
+
+	session.add(plazo)
+
+	clients = []
+	for i in range(10):
+		c = Cliente()
+		c.razons = fake.company()
+		c.nif = generate_random_nif()
+		c.sociedad = sociedad
+		c.clasificacion = clasificacion
+		c.condicion = condicion
+		c.forma_pago = forma_pago
+
+		session.add(c)
+		clients.append(c)
+
+	for c in clients:
+		c.direcciones.extend(
+			[
+				Direccion(
+					tdireccion=0,
+					domicilio=fake.street_address(),
+					ciudad=fake.city(),
+					prov=fake.state(),
+					pais=fake.country(),
+				),
+				Direccion(
+					tdireccion=1,
+					domicilio=fake.street_address(),
+					ciudad=fake.city(),
+					prov=fake.state(),
+					pais=fake.country(),
+				)
+			]
+		)
+
+
+if __name__ == '__main__':
+	Base.metadata.create_all(engine)
+	create_sample_data()
+	session.commit()
+
+
 
 
