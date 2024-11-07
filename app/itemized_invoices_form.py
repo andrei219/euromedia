80da96dd24753e4c3f43e6c5e6854699440ef65c
Andrei Enache <andrei.officee@gmail.com>
2024-11-07 10:10:37 +0100

    Remove debugging information from itemized_invoices_form

--- app/itemized_invoices_form.py
+++ app/itemized_invoices_form.py
@@ -22,33 +22,6 @@ import itertools as it
 class FilterValidationError(Exception):
     pass 
 
-sql = """
-    select 
-    prt.fiscal_name, 
-    si.TYPE, 
-    si.number, 
-    si.date, 
-    CONCAT_WS(' ',
-        IFNULL(i.mpn, ''),
-        IFNULL(i.manufacturer, ''),
-        IFNULL(i.category, ''),
-        IFNULL(i.model, ''),
-        IF(i.capacity IS NOT NULL, CONCAT(i.capacity, ' GB'), ''),
-        IFNULL(i.color, '')
-    ) as clean_repr,
-    spl.condition, 
-    spl.spec, 
-    es.serie, 
-    spl.price
-    from expedition_series es 
-    inner join expedition_lines el on es.line_id = el.id
-    inner join items i on el.item_id = i.id
-    inner join expeditions e on el.expedition_id = e.id
-    inner join sale_proformas sp on sp.id = e.proforma_id
-    inner join sale_proforma_lines spl on spl.proforma_id=sp.id
-    inner join partners prt on prt.id = sp.partner_id 
-    inner join sale_invoices si on si.id = sp.sale_invoice_id
-"""
 
 HEADER = ('Partner', 'Type', 'Number', 'Date', 'Item', 'Condition', 'Spec', 'Serie', 'Price')
 
@@ -85,18 +58,13 @@ class Form(Ui_Dialog, QDialog):
         utils.setCompleter(self.partner, tuple(utils.partner_id_map.keys()) + ('All', ))
 
         self._export.clicked.connect(self.export_handler)
-        self.series_all.toggled.connect(self.all_checked)
+        self.series_all.toggled.connect(self.check_all)
 
-        if os.getenv('APP_DEBUG') == 'true': 
-            self._from.setText("01012024") 
-        else:
-            self._from.setText(f"01{utils.today_date()[2:]}")
-    
+        self._from.setText(f"01{utils.today_date()[2:]}")
         self.to.setText(utils.today_date())
 
 
-
-    def all_checked(self, checked):
+    def check_all(self, checked):
         for serie in range(1, 7):
             getattr(self, 'serie_' + str(serie)).setChecked(checked)
     
@@ -165,7 +133,7 @@ class Form(Ui_Dialog, QDialog):
                 on el.expedition_id = e.id
             inner join sale_proformas sp
                 on e.proforma_id = sp.id
-            inner join ( -- build sale lines from both types: advanced and normal 
+            inner join (        -- build sale lines from both types: advanced and normal 
                 select 
                     proforma_id,
                     item_id,
